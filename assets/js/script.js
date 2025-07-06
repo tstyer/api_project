6bd09dcc9427edb820107139e4828f79bc9b4304
tstyer <travisstyer.ts@gmail.com>
2025-07-06 16:39:58 +0100

    Add first consts of js

---
+++ assets/js/script.js
@@ -0,0 +1,5 @@
+const API_Key = "PA_E4YJs_MnE4dWVR-qHjTutMvs";
+const API_URL = "https://ci-jshint.herokuapp.com/api";
+const resultsModal = new bootstrap.Modal(document.getElementById("resultsModal"));
+
+
