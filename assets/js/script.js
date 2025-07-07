2132575579e1d35b613aa0f5076c354f3204aef7
tstyer <travisstyer.ts@gmail.com>
2025-07-07 10:42:48 +0100

    Add display status function

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,17 +1,33 @@
-const API_Key = "PA_E4YJs_MnE4dWVR-qHjTutMvs";
+const API_KEY = "VZn-Fx2ohpPGHAs_RhP_m6IgxYA";
 const API_URL = "https://ci-jshint.herokuapp.com/api";
 const resultsModal = new bootstrap.Modal(document.getElementById("resultsModal"));
 
 document.getElementById("status").addEventListener("click", e => getStatus(e));
 
 async function getStatus(e) {
-    const queryString = '${API_URL}?api_key=${API_Key}';
 
-    const = await fetch(queryString);
+    const queryString = `${API_URL}?api_key=${API_KEY}`;
+
+    const response = await fetch(queryString);
 
     const data = await response.json();
 
     if (response.ok) {
-        console.log(data);
+        displayStatus(data);
+    } else {
+        throw new Error(data.error);
     }
+
+}
+
+function displayStatus(data) {
+
+    let heading = "API Key Status";
+    let results = `<div>Your key is valid until</div>`;
+    results += `<div class="key-status">${data.expiry}</div>`;
+
+    document.getElementById("resultsModalTitle").innerText = heading;
+    document.getElementById("results-content").innerHTML = results;
+    resultsModal.show();
+
 }
