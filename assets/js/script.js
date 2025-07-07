d00bf8c8f0b8939b948d87752088e3ae1747270d
tstyer <travisstyer.ts@gmail.com>
2025-07-07 10:29:44 +0100

    add fucntion to retrieve and display api key data

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,4 +2,16 @@ const API_Key = "PA_E4YJs_MnE4dWVR-qHjTutMvs";
 const API_URL = "https://ci-jshint.herokuapp.com/api";
 const resultsModal = new bootstrap.Modal(document.getElementById("resultsModal"));
 
+document.getElementById("status").addEventListener("click", e => getStatus(e));
 
+async function getStatus(e) {
+    const queryString = '${API_URL}?api_key=${API_Key}';
+
+    const = await fetch(queryString);
+
+    const data = await response.json();
+
+    if (response.ok) {
+        console.log(data);
+    }
+}
